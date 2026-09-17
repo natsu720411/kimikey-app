e3fcba670542d7b3b78ccd1e2a917e9b70d77e9c
natsu720411 <koyo0411koyo@gmail.com>
2026-09-17 21:19:31 +0900

    Update generate-seo-pages-2.mjs

--- scripts/generate-seo-pages-2.mjs
+++ scripts/generate-seo-pages-2.mjs
@@ -126,7 +126,15 @@ const pages = [
 
     ],
 
-    related: [
+        related: [
+
+      {
+        href:
+          '/pitch-training/',
+
+        label:
+          '音程を合わせる練習をする',
+      },
 
       {
         href:
